8e69c7fea8808a4e8c4d5772a18497368fe4428e
pavanthanay <pavanthanaym11@gmail.com>
2023-02-17 11:48:22 +0530

    Generalized Cards

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Display() 
+export default function Display(props) 
 {
   return (
     <div className="col">
@@ -8,9 +9,9 @@ export default function Display()
     
     <div className="card" style={{width: '18rem'}}>
       {/* <div className="card" style="width:18rem;"> */}
-      <img src="https://cdn.zeebiz.com/sites/default/files/2022/09/13/200028-untitled-design-2022-09-13t125137903.jpg" className="card-img-top" alt="..."/>
+      <img src={props.image} className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">{props.title}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="https://getbootstrap.com/docs/5.3/components/card/" className="btn btn-primary">Participate</a>
       </div>
@@ -20,3 +21,14 @@ export default function Display()
 }
 // How to handle events
 // How to set state
+
+Display.propTypes = {
+    title : PropTypes.string,
+    image : PropTypes.string
+    // title : PropTypes.string.isRequired
+}
+
+Display.defaultProps = {
+    title : "Contest",
+    image : "https://cdn.zeebiz.com/sites/default/files/2022/09/13/200028-untitled-design-2022-09-13t125137903.jpg"
+}
